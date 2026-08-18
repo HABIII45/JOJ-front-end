ed2f4d9c65eef386ac85079603332afe6be70561
desire427 <alphonsedesirehaba427@gmail.com>
2026-08-18 22:23:59 +0000

    version 1.0.2

--- JOJ-frontend/src/routes/AppRoutes.jsx
+++ JOJ-frontend/src/routes/AppRoutes.jsx
@@ -11,6 +11,7 @@ const AppRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register-result" element={<RegisterResultPage />} />
       <Route path="/payment/summary" element={<PaymentSummary />} />
+      <Route path="*" element={<Navigate to="/register-result" replace />} />
     </Routes>
   );
 };
