7786b5945690314e838ea762e89d57e8192d72c6
desire427 <alphonsedesirehaba427@gmail.com>
2026-08-18 09:56:22 +0000

    version 1.0.0 Connexion static

--- JOJ-frontend/src/routes/AppRoutes.jsx
+++ JOJ-frontend/src/routes/AppRoutes.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PaymentSummary from '../pages/public/PaymentSummary';
+import LoginPage from '../pages/public/LoginPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/payment/summary" element={<PaymentSummary />} />
     </Routes>
   );
