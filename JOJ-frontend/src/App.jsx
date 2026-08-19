cd420e892b21182dcb6c3bca2263f356dcb5adb6
habii45 <habyl5430@gmail.com>
2026-08-19 17:20:14 +0000

    feat: mettre à jour App

--- JOJ-frontend/src/App.jsx
+++ JOJ-frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // import { Footer } from "./components/layout/Footer"
 // import { Header } from "./components/layout/Header"
@@ -14,30 +13,6 @@ function App() {
       {/* <Footer /> */}
 
     </>
-=======
-/**
- * Routage JOJ_Events :
- *  - Routes publiques (page d'accueil + sections) avec PublicLayout
- *  - Route /dashboard avec AdminLayout
- * Design : référence maquettes fournies (orange #C25B1E, noir, Poppins).
- * @module App
- */
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/admin/Dashboard";
-import Disciplines from "./pages/admin/Games";
-import AdminSites from "./pages/admin/AdminSites";
-import SiteDetail from "./pages/admin/SitesDetail";
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path={"/disciplines"} element={<Disciplines />} />
-        <Route path={"/sites"} element={<AdminSites />} />
-        <Route path="/sites/:id" element={<SiteDetail />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> origin/feature/admin-liste-sites
   );
 }
 
